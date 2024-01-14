4272713ee079830eeeb51ac6fac7af4d51058aa6
Clement Chau <142649232+Realify2005@users.noreply.github.com>
2024-01-14 19:51:09 +1100

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-live: 
+live: https://realify2005.github.io/weather-app
+
+This project is solely with the intent of learning javascript, I have not designed to make it look nice, only added functionalities.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-alert("Hello");
+async function fetchData(city) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=b08cce43980e41f7bb580301241401&q=${city}`
+    );
+
+    if (!response.ok) {
+      throw new Error("something went wrong");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+const body = document.querySelector("body");
+
+const h1 = document.createElement("h1");
+h1.textContent = "Weather checker";
+
+const input = document.createElement("input");
+input.placeholder = "Enter city";
+
+const button = document.createElement("button");
+button.textContent = "Search";
+
+const div = document.createElement("div");
+
+body.appendChild(h1);
+body.appendChild(input);
+body.appendChild(button);
+
+const temp = document.createElement("p");
+const condition = document.createElement("p");
+const humidity = document.createElement("p");
+
+async function handleButtonClick() {
+  const data = await fetchData(input.value);
+
+  temp.textContent = `Temperature: ${data.current.temp_c} degrees Celcius`;
+  condition.textContent = `Condition: ${data.current.condition.text}`;
+  humidity.textContent = `Humidity: ${data.current.humidity}%`;
+}
+
+button.addEventListener("click", handleButtonClick);
+
+div.appendChild(temp);
+div.appendChild(condition);
+div.appendChild(humidity);
+body.appendChild(div);
